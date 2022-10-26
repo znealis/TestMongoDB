391147014eaf518d4e550c1328245c1deb034153
Chrissie Zimmer <czimmer12@aol.com>
2022-10-26 15:34:37 -0400

    Server + Single Web Page

---
+++ index.cjs
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs = require('fs');
+const port = 8080;
+
+http.createServer(function (req, res) {
+  fs.readFile('index.html', function(err, data){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(port);
+console.log("serving on: localhost:", port);
