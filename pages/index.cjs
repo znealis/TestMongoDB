52b113763a82752f5a3c93cd39073e1a08ab97ba
Chrissie Zimmer <czimmer12@aol.com>
2022-10-26 16:41:43 -0400

    possible

--- pages/index.cjs
+++ pages/index.cjs
@@ -4,11 +4,16 @@ var url = require('url');
 const port = 8080;
 
 http.createServer(function (req, res) {
-  fs.readFile('index.html', function(err, data){
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
-
 }).listen(port);
 console.log("serving on: localhost:", port);
