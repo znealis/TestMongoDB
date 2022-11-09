17430e5b35a3766cb36b9b16caba402ae02e0c25
Chrissie Zimmer <czimmer12@aol.com>
2022-11-09 14:00:27 -0500

    restructured

---
+++ src/pages/index.cjs
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+const port = 8080;
+
+http.createServer(function (req, res) {
+  fs.readFile('./pages/index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(port);
+console.log("serving on: localhost:", port);
