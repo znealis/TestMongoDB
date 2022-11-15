eb25d66d755acf752968033b1e6e3ea94db5dab0
Zach <z.nealis@gmail.com>
2022-11-15 13:47:02 -0500

    changed back

--- src/pages/index.cjs
+++ src/pages/index.cjs
@@ -4,7 +4,7 @@ var url = require('url');
 const port = 8080;
 
 http.createServer(function (req, res) {
-  fs.readFile('./src/pages/index.html', function(err, data) {
+  fs.readFile('./pages/index.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
