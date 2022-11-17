30425416d4ff4a7c2bf00131755368ca131dadf2
Zach <z.nealis@gmail.com>
2022-11-17 08:45:46 -0500

    zach.ejs works now

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true})
             })      
          })
 
-    app.get('/christina', (req, res) => {
+    app.get('/christina.ejs', (req, res) => {
         db.collection('newdata').aggregate([{ $unwind: '$results'},
         {$match: {'results.artistName' : 'Drake'}},
         {$group: {'_id': null, 'songs': {$push: {'artist': '$results.artistName', 'track': '$results.trackName'}}}},
@@ -37,7 +37,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true})
             res.render('christina.ejs', {results: unwind(results, 'songs')})
         })
     })
-        app.get('/nicole', (req, res) => {
+        app.get('/nicole.ejs', (req, res) => {
             db.collection('newdata').aggregate([{ $unwind: '$results'},
             {$match: {'results.artistName' : 'Daft Punk'}},
             {$group: {'_id': null, 'songs': {$push: {'artist': '$results.artistName', 'track': '$results.trackName'}}}},
@@ -48,7 +48,7 @@ MongoClient.connect(uri, { useUnifiedTopology: true})
                 res.render('nicole.ejs', {results: unwind(results, 'songs')})
             })
         })
-            app.get('/john', (req, res) => {
+            app.get('/john.ejs', (req, res) => {
                 db.collection('newdata').aggregate([{ $unwind: '$results'},
                 {$match: {'results.artistName' : 'Avenged'}},
                 {$group: {'_id': null, 'songs': {$push: {'artist': '$results.artistName', 'track': '$results.trackName'}}}},
@@ -59,15 +59,26 @@ MongoClient.connect(uri, { useUnifiedTopology: true})
                     res.render('john.ejs', {results: unwind(results, 'songs')})
                 })
             })
-                app.get('/zach', (req, res) => {
-                    db.collection('newdata').aggregate([{ $unwind: '$results'},
-                    {$match: {'results.artistName' : 'Hannah Montana'}},
-                    {$group: {'_id': null, 'songs': {$push: {'artist': '$results.artistName', 'track': '$results.trackName'}}}},
-                    {$project: {'_id': 0, 'songs': 1}}]).toArray()
+                app.get('/zach.ejs', (req, res) => {
+                    db.collection('newdata').aggregate([{$unwind: '$topCharts.data'},
+                    {$match: {'topCharts.data.title' : "God's Plan"}},
+                    {$group: {'_id': null, 'songDetails': {$push:{
+                        'title': '$topCharts.data.title',
+                        'artist': '$topCharts.data.artist.name',
+                        'album': '$topCharts.data.album.title',
+                        'albumCover': '$topCharts.data.album.cover',
+                        'preview': '$topCharts.data.preview', 
+                        'Link': '$topCharts.data.link',
+                        'mediumPicture': '$topCharts.data.artist.picture_medium'}}
+                    }},
+                    {$project:{
+                        'songDetails' : 1,
+                        '_id': 0,
+                    }}]).toArray()
         
                     .then(results => {
-                        console.log(unwind(results, 'songs'))
-                        res.render('zach.ejs', {results: unwind(results, 'songs')})
+                        console.log(unwind(results, 'songDetails'))
+                        res.render('zach.ejs', {results: unwind(results, 'songDetails')})
                     })
                 })
 
